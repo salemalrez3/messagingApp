841b22f9ac69723f56572b947de704a85600f7ea
salem alrez <salem.seraj@gmail.com>
2025-11-30 21:51:22 +0300

    added the routes :> :>

--- backend/src/routes/user.routes.ts
+++ backend/src/routes/user.routes.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
-import { register, requestOtp, verifyOtp } from "../controllers/user.controller";
+import { forgotPassword, register, requestOtp, resetPassword, verifyOtp } from "../controllers/user.controller";
 
 const router = Router();
 
 router.post("/register",register );
 router.post("/requestOtp", requestOtp);
 router.post("/verifyOtp", verifyOtp);
+router.post("/forgotPassword", forgotPassword);
+router.post("/resetPassword", resetPassword);
 
 export default router;  
